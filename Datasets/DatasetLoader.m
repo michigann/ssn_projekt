e800cf64750f0f884c2d24712e7951a10cc1b7b5
Michał Kuklewski <kuklewski.michal@gmail.com>
2018-06-16 23:06:59 +0200

    datasets

--- Datasets/DatasetLoader.m
+++ Datasets/DatasetLoader.m
@@ -10,27 +10,30 @@ function [X, T] = DatasetLoader(dataset)
             [X, T] = loadHTRU2Dataset();
         case 'arrhythmia'
             [X, T] = loadArrhythmiaDataset();
-        case 'wine'
-            [X, T] = loadWineDataset();
-        case 'iris'
-            [X, T] = loadIrisDataset();
-        case 'wine'
-            [X, T] = loadWineDataset();
-        case 'iris'
-            [X, T] = loadIrisDataset();
+        case 'votes'
+            [X, T] = loadCongressionalVotingsDataset();
+        case 'ovariancancer'
+            [X, T] = loadOvariancancerDataset();
+        case 'glass'
+            [X, T] = loadGlassDataset();
+        case 'yeast'
+            [X, T] = loadYeastDataset();
         case 'wine'
             [X, T] = loadWineDataset();    
     end
 end
 
+%[3]
 function [X, T] = loadIrisDataset()
     [X, T] = iris_dataset;
 end
 
+%[6]
 function [X, T] = loadWineDataset()
     [X, T] = wine_dataset;
 end
 
+%[4]
 function [X, T] = loadIonosphereDataset()
     load ionosphere;
     t = char(Y)';
@@ -39,12 +42,14 @@ function [X, T] = loadIonosphereDataset()
     T = str2num(t');
 end
 
+%[10]
 function [X, T] = loadHTRU2Dataset()
-    load '/Datasets/HTRU_2/HTRU_2.csv'
+    load Datasets/HTRU_2/HTRU_2.csv
     X = HTRU_2(:,1:end-1);
     T = HTRU_2(:,end);
 end
 
+%[8]
 function [X, T] = loadArrhythmiaDataset()
     load arrhythmia;
     X = X';
@@ -57,5 +62,108 @@ function [X, T] = loadArrhythmiaDataset()
     targets = targets';
     T = targets;
 end
+
+function [X, T] = loadCongressionalVotingsDataset()
+    f = fopen('Datasets/CongressionalVotings/house-votes-84.data');
+    
+    X = [];
+    T = [];
+
+    tline = fgetl(f);
+    while ischar(tline)
+        tline = strrep(tline,',y',',1');
+        tline = strrep(tline,',n',',0');
+        tline = strrep(tline,',?',',0.5');
+        tline = strsplit(tline, ',');
+       
+        X = [X strcmp(tline{1}, 'democrat')];
+       
+        tline = tline(2:end);
+        tline = cellfun(@str2num, tline);
+        T = [T; tline];  
+        tline = fgetl(f);
+    end
+    T = T';
+    fclose(f);
+end
+
+function [X, T] = loadOvariancancerDataset()
+    load ovariancancer.mat;
+    inputs = obs';
+    targets = zeros(1, size(grp, 1));
+    for i=1:size(grp, 1)
+       if grp{i} == 'Cancer'
+           targets(i) = 1;
+       else
+           targets(i) = 0;
+       end
+    end
+    X = inputs;
+    T = targets;
+end
+
+function [X, T] = loadGlassDataset()
+    load Datasets/Glass/glass.data;
+
+    inputs = glass(:, 2:size(glass, 2)-1)';
+    Y = glass(:, size(glass, 2));
+    binDim = length(dec2bin(max(Y)));
+    Y = dec2bin(Y, binDim);
+    targets = zeros(size(Y, 1), binDim);
+    for i=1:size(targets, 1)
+        targets(i, : ) = str2num(Y(i, : )')';
+    end
+    targets = targets';
+    
+    X = inputs;
+    T = targets;
+end
+
+function [X, T] = loadYeastDataset()
+    f = fopen('Datasets/Yeast/yeast.data');
+    
+    X = [];
+    T = [];
+
+    tline = fgetl(f);
+    while ischar(tline)
+        tline = strsplit(tline);
+        
+        switch tline{end}
+            case 'CYT'
+                t = [0 0 0 0 0 0 0 0 0 1];
+            case 'NUC'
+                t = [0 0 0 0 0 0 0 0 1 0];
+            case 'MIT'
+                t = [0 0 0 0 0 0 0 1 0 0];
+            case 'ME3'
+                t = [0 0 0 0 0 0 1 0 0 0];
+            case 'ME2'
+                t = [0 0 0 0 0 1 0 0 0 0];
+            case 'ME1'
+                t = [0 0 0 0 1 0 0 0 0 0];
+            case 'EXC'
+                t = [0 0 0 1 0 0 0 0 0 0];
+            case 'VAC'
+                t = [0 0 1 0 0 0 0 0 0 0];
+            case 'POX'
+                t = [0 1 0 0 0 0 0 0 0 0];
+            case 'ERL'
+                t = [1 0 0 0 0 0 0 0 0 0];
+        end
+        T = [T; t];
+        
+        x = cellfun(@str2num, tline(2:end-1));
+        X = [X; x];
+        
+        tline = fgetl(f);
+    end
+    
+    fclose(f); 
+    
+    X = X';
+    T = T';
+end
+
 
 
